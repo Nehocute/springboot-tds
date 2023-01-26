6f2863d98d4d78eb92b7b6fe4004d7b4fd90637c
Nehocute <bukin211@campus.unicaen.fr>
2023-01-26 15:09:11 +0100

    feat: add form to add item to list

--- src/main/kotlin/edu/spring/td1/controller/ItemsController.kt
+++ src/main/kotlin/edu/spring/td1/controller/ItemsController.kt
@@ -2,9 +2,8 @@ package edu.spring.td1.controller
 
 import edu.spring.td1.models.Item
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.SessionAttributes
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.view.RedirectView
 
 
 @Controller
@@ -15,15 +14,24 @@ class ItemsController {
     val items: Set<Item>
         get() {
             var items=HashSet<Item>()
-            items.add(Item("item"))
+            items.add(Item("itm"))
             return items
         }
-
-
 
     @RequestMapping("/")
     fun indexAction():String{
         return "index"
+    }
+
+    @GetMapping("/new")
+    fun newAction():String{
+        return "newForm"
+    }
+
+    @PostMapping("/addNew")
+    fun addNewAction(@ModelAttribute("nom")nom:String, @SessionAttribute("items")items: HashSet<Item>): RedirectView {
+        items.add(Item(nom))
+        return RedirectView("/")
     }
 
 }
