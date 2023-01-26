4f981b79b1753b07b440ffab0e2d16a8c513ccf1
Nehocute <bukin211@campus.unicaen.fr>
2023-01-26 14:38:03 +0100

    feat: show item

--- src/main/kotlin/edu/spring/td1/controller/ItemsController.kt
+++ src/main/kotlin/edu/spring/td1/controller/ItemsController.kt
@@ -1,11 +1,25 @@
 package edu.spring.td1.controller
 
+import edu.spring.td1.models.Item
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.SessionAttributes
 
 
 @Controller
+@SessionAttributes("items")
 class ItemsController {
+
+    @get:ModelAttribute("items")
+    val items: Set<Item>
+        get() {
+            var items=HashSet<Item>()
+            items.add(Item("item"))
+            return items
+        }
+
+
 
     @RequestMapping("/")
     fun indexAction():String{
