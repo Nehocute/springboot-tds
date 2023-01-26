948f1c383966cba886416f7924bc61551e7a674d
Nehocute <bukin211@campus.unicaen.fr>
2023-01-26 14:11:30 +0100

    working project

---
+++ src/main/kotlin/edu/spring/td1/controller/ItemsController.kt
@@ -0,0 +1,15 @@
+package edu.spring.td1.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+
+@Controller
+class ItemsController {
+
+    @RequestMapping("/")
+    fun indexAction():String{
+        return "index"
+    }
+
+}
